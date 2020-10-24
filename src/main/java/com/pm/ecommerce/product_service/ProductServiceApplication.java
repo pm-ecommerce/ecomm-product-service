f904e3ac249ba644d7aa868aa5b0c758494581ba
Sandeep Giri <ioesandeep@gmail.com>
2020-10-24 16:57:29 -0400

    Spring cloud config

--- src/main/java/com/pm/ecommerce/product_service/ProductServiceApplication.java
+++ src/main/java/com/pm/ecommerce/product_service/ProductServiceApplication.java
@@ -3,9 +3,11 @@ package com.pm.ecommerce.product_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.pm.ecommerce.entities"})
+@EnableDiscoveryClient
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
